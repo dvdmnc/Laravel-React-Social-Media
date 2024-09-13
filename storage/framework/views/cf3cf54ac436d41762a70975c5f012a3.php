f978863c32b0c42b086e6b237160d6b54c41292d
dvdmnc <130088298+dvdmnc@users.noreply.github.com>
2024-09-13 16:46:12 +0200

    Delete storage/framework/views directory

--- storage/framework/views/cf3cf54ac436d41762a70975c5f012a3.php
+++
@@ -1,3 +0,0 @@
-<?php echo e($slot); ?>: <?php echo e($url); ?>
-
-<?php /**PATH C:\Users\dmene\Documents\Social Media Laravel & React\social media laravel react\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/header.blade.php ENDPATH**/ ?>
--- storage/framework/views/e8beebe9f88f6f1be07297536a86a598.php
+++
@@ -1,3 +0,0 @@
-<?php echo e($slot); ?>
-
-<?php /**PATH C:\Users\dmene\Documents\Social Media Laravel & React\social media laravel react\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/footer.blade.php ENDPATH**/ ?>
